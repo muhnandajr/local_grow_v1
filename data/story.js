c55ceaeb41ccb1a9e0141bc111c8793a387854a3
M Nanda J R <nandajabar@Ms-MacBook-Pro.local>
2022-12-25 22:04:53 +0700

    add all resources

---
+++ data/story.js
@@ -0,0 +1,143 @@
+var story = {
+ "docName": "Demo Station (Community)",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "serverToolsPath": "",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableInteractions": false,
+ "disableHotspots": false,
+ "hideGallery": false,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": true,
+ "highlightLinks": true,
+ "pages": [
+  {
+   "id": "1:206",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Desktop - Local Grow",
+   "image": "desktop-local-grow.png",
+   "imageFull": "full/desktop-local-grow.png",
+   "index": 0,
+   "width": 1440,
+   "height": 3693,
+   "x": -271,
+   "y": -1891,
+   "type": "regular",
+   "fixedPanels": [
+    {
+     "constrains": {
+      "top": false,
+      "bottom": false,
+      "left": false,
+      "right": false,
+      "height": false,
+      "width": false
+     },
+     "x": 0,
+     "y": 0,
+     "width": 1440,
+     "height": 151,
+     "type": "float",
+     "index": 0,
+     "isFloat": true,
+     "isVertScroll": false,
+     "links": [],
+     "image": "desktop-local-grow-0.png",
+     "mskH": 151
+    }
+   ],
+   "links": [
+    {
+     "rect": {
+      "x": 224,
+      "y": 1307,
+      "width": 196,
+      "height": 57
+     },
+     "index": 0,
+     "url": "https://calendly.com/localgrow/diagnostic"
+    },
+    {
+     "rect": {
+      "x": 1030,
+      "y": 1307,
+      "width": 196,
+      "height": 57
+     },
+     "index": 1,
+     "url": "https://forms.gle/hZnFYvRjLzFzVxwY7"
+    },
+    {
+     "rect": {
+      "x": 628,
+      "y": 1307,
+      "width": 196,
+      "height": 57
+     },
+     "index": 2,
+     "url": "https://forms.gle/hZnFYvRjLzFzVxwY7"
+    },
+    {
+     "rect": {
+      "x": 847,
+      "y": 2706,
+      "width": 408,
+      "height": 57
+     },
+     "index": 3,
+     "url": "https://forms.gle/hZnFYvRjLzFzVxwY7"
+    },
+    {
+     "rect": {
+      "x": 938,
+      "y": 3183,
+      "width": 302,
+      "height": 62
+     },
+     "index": 4,
+     "url": "https://calendly.com/localgrow/diagnostic"
+    },
+    {
+     "rect": {
+      "x": 1209,
+      "y": 3593,
+      "width": 40,
+      "height": 40
+     },
+     "index": 5,
+     "url": "https://www.linkedin.com/in/muhnandajr/"
+    },
+    {
+     "rect": {
+      "x": 1265,
+      "y": 3593,
+      "width": 40,
+      "height": 40
+     },
+     "index": 6,
+     "url": "https://www.instagram.com/localgrow_id/"
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "0:1",
+   "name": "Page 1"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 1,
+ "backColor": "#FFFFFF"
+}
